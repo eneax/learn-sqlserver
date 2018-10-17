1dd81d30c7133bbdb7cb3882561a5fb2256c6c27
eneax <19818937+eneax@users.noreply.github.com>
2018-10-17 18:28:50 +0200

    Intro to Aliases and Identifiers

--- SELECT Clause.sql
+++ SELECT Clause.sql
@@ -20,3 +20,28 @@ SELECT [TransactionID],
 	   'Batch 1' AS [BatchID],
 	   ([Quantity] * [ActualCost]) AS [TotalCost]
 FROM [Production].[TransactionHistory];
+
+
+
+-- Demo: Column Aliases
+SELECT [Name] AS [DepartmentName], -- recommended approach
+	   [Name] [DepartmentName],    -- not recommended
+	   [GroupName] AS [GN]
+FROM [HumanResources].[Department];
+
+
+
+-- Demo: Regular vs Delimited Identifiers
+
+-- Regular vs Delimited (in this case, both are allowed)
+SELECT Name,
+	   [Name]
+FROM [HumanResources].[Department];
+
+-- Create a temporary table
+CREATE TABLE #Department
+	([Department ID] INT NOT NULL);
+GO
+
+SELECT [Department ID]
+FROM [#Department];
