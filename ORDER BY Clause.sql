26a08b9c488a9376ddf5d268f3a25371eec8c63c
eneax <19818937+eneax@users.noreply.github.com>
2018-10-22 10:33:23 +0200

    Add Query Paging

--- ORDER BY Clause.sql
+++ ORDER BY Clause.sql
@@ -19,3 +19,28 @@ SELECT [sod].[SpecialOfferID],
 	   [sod].[ProductID]
 FROM [Sales].[SalesOrderDetail] AS [sod]
 ORDER BY 1, 2;
+
+
+
+
+-- Demo: Query Paging
+
+-- Returning the first 25 rows
+SELECT [e].[FirstName],
+		[e].[LastName],
+		[e].[AddressLine1]
+FROM [HumanResources].[vEmployee] AS [e]
+ORDER BY [e].[LastName], [e].[FirstName]
+	OFFSET 0 ROWS
+	FETCH NEXT 25 ROWS ONLY;
+	-- just like using TOP 25
+
+-- Paging through the next 25 rows
+SELECT [e].[FirstName],
+		[e].[LastName],
+		[e].[AddressLine1]
+FROM [HumanResources].[vEmployee] AS [e]
+ORDER BY [e].[LastName], [e].[FirstName]
+	OFFSET 25 ROWS
+	FETCH NEXT 25 ROWS ONLY;
+	-- we skip the first 25 rows
