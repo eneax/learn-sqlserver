113155c2de0a6f045a2c922526d272a40d4b6d37
eneax <19818937+eneax@users.noreply.github.com>
2018-10-22 08:56:02 +0200

    Add DISTINCT examples

--- WHERE Clause.sql
+++ WHERE Clause.sql
@@ -38,3 +38,30 @@ SELECT [sod].[SalesOrderID],
 	   [sod].[ProductID]
 FROM [Sales].[SalesOrderDetail] AS [sod]
 WHERE NOT [sod].[ProductID] = 711;
+
+
+
+-- Demo: DISTINCT
+-- If you add DISTINCT to a SELECT clause, it will remove duplicate rows from the final result set
+-- Use DISTINCT only if you really need it
+-- NULL is treated as a distinct value
+
+-- No DISTINCT (we will have 12 rows, all duplicates with SalesOrderID = 43659)
+SELECT [sod].[SalesOrderID]
+FROM [Sales].[SalesOrderDetail] AS [sod]
+WHERE [sod].[CarrierTrackingNumber] = '4911-403c-98';
+
+-- With DISTINCT (we will have only one row with SalesOrderID = 43659)
+SELECT DISTINCT [sod].[SalesOrderID]
+FROM [Sales].[SalesOrderDetail] AS [sod]
+WHERE [sod].[CarrierTrackingNumber] = '4911-403c-98';
+
+-- NULL handling
+SELECT DISTINCT [CarrierTrackingNumber]
+FROM [Sales].[SalesOrderDetail] AS [sod]
+ORDER BY [CarrierTrackingNumber];
+
+-- Count rows with NULL
+SELECT COUNT(*)
+FROM [Sales].[SalesOrderDetail] AS [sod]
+WHERE [CarrierTrackingNumber] IS NULL;
