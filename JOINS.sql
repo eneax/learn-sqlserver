e12c431d46cac07b1e3bdad7bfe4d9837d3a7e6d
eneax <19818937+eneax@users.noreply.github.com>
2018-10-23 10:04:26 +0200

    Add example for Equi vs. Non-Equi Joins

--- JOINS.sql
+++ JOINS.sql
@@ -133,3 +133,22 @@ GO
 
 
 
+-- Demo: Equi vs. Non-Equi Joins
+SELECT sod.SalesOrderID,
+	   sod.SalesOrderDetailID,
+	   sod.ProductID,
+	   sod.OrderQty,
+	   so.SpecialOfferID,
+	   sod.ModifiedDate,
+	   so.StartDate,
+	   so.EndDate,
+	   so.Description
+FROM [Sales].[SalesOrderDetail] AS sod
+INNER JOIN [Sales].[SpecialOffer] AS so
+ON so.SpecialOfferID = sod.SpecialOfferID
+	AND sod.ModifiedDate < so.EndDate
+	AND sod.ModifiedDate >= so.StartDate
+WHERE so.SpecialOfferID > 1;
+
+
+
