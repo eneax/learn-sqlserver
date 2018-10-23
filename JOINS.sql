01927344221fe7c5e6a204dc7ca240071958162f
eneax <19818937+eneax@users.noreply.github.com>
2018-10-23 10:09:08 +0200

    Add Multi-Attribute Joins

--- JOINS.sql
+++ JOINS.sql
@@ -152,3 +152,31 @@ WHERE so.SpecialOfferID > 1;
 
 
 
+-- Demo: Multi-Attribute Joins (it involves more than one column for each input)
+USE AdventureWorks2012;
+
+-- Creating a new table based on [Person].[BusinessEntityAddress]
+SELECT BusinessEntityID, AddressID,
+	   AddressTypeID, rowguid, ModifiedDate
+INTO [Person].[BusinessEntityAddressArchive]
+FROM [Person].[BusinessEntityAddress];
+
+-- Removing an arbitrary 1,500 rows fro the demo
+DELETE TOP (1500)
+FROM [Person].[BusinessEntityAddressArchive];
+
+-- Which rows are in the production table that are NOT in the archive table?
+SELECT bea.BusinessEntityID, bea.AddressID, bea.AddressTypeID,
+	   bea.rowguid, bea.ModifiedDate
+FROM [Person].[BusinessEntityAddress] AS bea
+LEFT OUTER JOIN [Person].[BusinessEntityAddressArchive] AS abea
+ON bea.BusinessEntityID = abea.BusinessEntityID AND
+	bea.AddressID = abea.AddressID AND
+	bea.AddressTypeID = abea.AddressTypeID
+WHERE abea.BusinessEntityID IS NULL;
+-- it shows the 1500 rows that I deleted previously
+
+-- Demo Cleanup
+DROP TABLE [Person].[BusinessEntityAddressArchive];
+
+
