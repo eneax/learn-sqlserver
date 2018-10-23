3938084367d85f3eb4ec803ea55d0d1afc64c9ac
eneax <19818937+eneax@users.noreply.github.com>
2018-10-23 10:23:47 +0200

    Add demo for Joining More than Two Tables

--- JOINS.sql
+++ JOINS.sql
@@ -180,3 +180,36 @@ WHERE abea.BusinessEntityID IS NULL;
 DROP TABLE [Person].[BusinessEntityAddressArchive];
 
 
+
+-- Demo: Joining More than Two Tables
+USE AdventureWorks2012;
+
+-- INNER JOIN multi-table join example
+-- Shaw actual execution plan (compare logical to physical)
+SELECT p.Name AS [ProductName],
+	   pc.Name AS [CategoryName],
+	   ps.Name AS [SubcategoryName]
+FROM [Production].[Product] AS p
+INNER JOIN [Production].[ProductSubcategory] AS ps
+	ON p.ProductSubcategoryID = ps.ProductSubcategoryID
+INNER JOIN [Production].[ProductCategory] AS pc
+	ON ps.ProductSubcategoryID = pc.ProductCategoryID
+ORDER BY [ProductName], [CategoryName], [SubcategoryName];
+
+-- OUTER JOIN multi-table join example with logical issue
+SELECT p.Name, sod.SalesOrderDetailID
+FROM [Production].[Product] AS p
+LEFT OUTER JOIN [Sales].[SalesOrderDetail] AS sod
+	ON p.ProductID = sod.ProductID
+INNER JOIN [Sales].[SpecialOffer] AS so
+	ON sod.SpecialOfferID = so.SpecialOfferID
+ORDER BY p.Name, sod.SalesOrderDetailID;
+
+-- Fixing the logical issue
+SELECT p.Name, sod.SalesOrderDetailID
+FROM [Production].[Product] AS p
+LEFT OUTER JOIN [Sales].[SalesOrderDetail] AS sod
+	ON p.ProductID = sod.ProductID
+LEFT OUTER JOIN [Sales].[SpecialOffer] AS so
+	ON sod.SpecialOfferID = so.SpecialOfferID
+ORDER BY p.Name, sod.SalesOrderDetailID;
